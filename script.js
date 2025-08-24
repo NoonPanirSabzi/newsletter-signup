64ade0c429c7644a3b01d943d6b5db0a2a4c3c53
Amin Forouzan <amin.pqr@gmail.com>
2025-08-24 21:10:47 +0330

    feat: add form submit validation

---
+++ script.js
@@ -0,0 +1,36 @@
+const elements = {
+  subscribeForm: document.getElementById("subscribe-form"),
+  userEmail: document.getElementById("user-email"),
+  invalidInputNotif: document.getElementById("invalid-input-notif"),
+  subContainer: document.querySelector(".sub"),
+  messageContainer: document.querySelector(".message"),
+  dissmissBtn: document.getElementById("dismiss-btn"),
+  confirmMsg: document.getElementById("confirm-msg"),
+};
+
+const validEmailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+elements.subscribeForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const userInput = elements.userEmail.value;
+  if (validEmailRegex.test(userInput)) {
+    elements.subContainer.classList.add("hide");
+    elements.messageContainer.classList.remove("hide");
+    elements.confirmMsg.innerHTML = `A confirmation email has been sent to <b>${userInput}</b>.
+            Please open it and click the button inside to confirm your
+            subscription.`;
+    elements.invalidInputNotif.classList.add("hide");
+    elements.userEmail.classList.remove("invalid-input");
+  } else {
+    elements.invalidInputNotif.classList.remove("hide");
+    elements.userEmail.classList.add("invalid-input");
+  }
+});
+
+elements.dissmissBtn.addEventListener("click", () => {
+  elements.subContainer.classList.remove("hide");
+  elements.messageContainer.classList.add("hide");
+  elements.userEmail.value = "";
+  elements.confirmMsg.innerHTML = "";
+});
