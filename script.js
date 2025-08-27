7ea8e1352309261c1dc688df12b4f8ff78fcd18a
Amin Forouzan <amin.pqr@gmail.com>
2025-08-27 11:42:40 +0330

    refactor: Simplify email validation and page toggle logic

--- script.js
+++ script.js
@@ -10,29 +10,39 @@ const elements = {
 
 const validEmailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
+function validateEmail(userInput) {
+  if (validEmailRegex.test(userInput.value)) {
+    return true;
+  }
+  return false;
+}
+
+function togglePage(showPage, hidePage) {
+  showPage.classList.toggle("hide");
+  hidePage.classList.toggle("hide");
+  elements.messageContainer.classList.toggle("show");
+}
+
 elements.subscribeForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const userInput = elements.userEmail.value;
-  if (validEmailRegex.test(userInput)) {
-    elements.subContainer.classList.add("hide");
-    elements.messageContainer.classList.remove("hide");
-    elements.messageContainer.classList.add("show")
-    elements.confirmMsg.innerHTML = `A confirmation email has been sent to <b>${userInput}</b>.
+  const userInput = elements.userEmail;
+  if (validateEmail(userInput)) {
+    togglePage(elements.messageContainer ,elements.subContainer)
+
+    elements.confirmMsg.innerHTML = `A confirmation email has been sent to <b>${userInput.value}</b>.
             Please open it and click the button inside to confirm your
             subscription.`;
     elements.invalidInputNotif.classList.add("hide");
-    elements.userEmail.classList.remove("invalid-input");
+    userInput.classList.remove("invalid-input");
   } else {
     elements.invalidInputNotif.classList.remove("hide");
-    elements.userEmail.classList.add("invalid-input");
+    userInput.classList.add("invalid-input");
   }
 });
 
 elements.dissmissBtn.addEventListener("click", () => {
-  elements.subContainer.classList.remove("hide");
-  elements.messageContainer.classList.add("hide");
-  elements.messageContainer.classList.remove("show")
+  togglePage(elements.subContainer, elements.messageContainer)
   elements.userEmail.value = "";
   elements.confirmMsg.innerHTML = "";
 });
